2d9b7ca41ec557babf07ee1256f5e89ae6d2d221
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-18 10:02:15 +0000

    created middleware for handling errors on unknown endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,5 @@
+import "dot/env"
+import route from "./routes/users"
 import express {Request, Response, NextFunction} from "express"
 import createHttpError, { isHttpError} from "http-errors"
 
@@ -5,5 +7,9 @@ import createHttpError, { isHttpError} from "http-errors"
 const app = express()
 
 app.use(express.json())
+
+app.use("/api/users", route)
+
+app.use()
 
 export default app
