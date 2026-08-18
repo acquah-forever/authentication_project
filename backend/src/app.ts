11fc2dd639e8e37607b0366bf2052b75e6fe93e3
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-18 10:39:43 +0000

    connected backend to mongo db database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,55 @@
-import "dot/env"
-import route from "./routes/users"
-import express {Request, Response, NextFunction} from "express"
-import createHttpError, { isHttpError} from "http-errors"
+import "dotenv/config"
+import express, { Request, Response, NextFunction } from "express"
+import createHttpError, { isHttpError } from "http-errors"
+
+import session from "express-session";
+import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
 
 
 const app = express()
 
 app.use(express.json())
 
-app.use("/api/users", route)
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend is running successfully"
+  });
+});
 
-app.use()
+app.use(session({
+  secret: env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+  },
+  rolling: true, // Reset the cookie expiration time on every request
+
+  // where session data will be stored
+  store: MongoStore.create({
+    mongoUrl: env.MONGO_CONNECTION_STRING,
+    collectionName: "sessions",
+  }),
+}));
+
+
+
+app.use(( req, res, next) => {
+    next(createHttpError(404, "Endpoint not found"))
+})
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+
+  let statusCode = 500;
+  let errorMessage = "An unknown error occurred";
+
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+
+  res.status(statusCode).json({ error: errorMessage });
+});
 
 export default app
