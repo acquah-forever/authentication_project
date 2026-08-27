f95f4566a2c2a95bf8b637322e03893f5e32d2ea
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-27 15:49:32 +0000

    added dropdown component on more button in navbar

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,7 +1,7 @@
 
 const Hero = () => {
     return (
-        <div className="flex flex-col justify-center items-center px-5 min-h-screen">
+        <div className="flex flex-col justify-center items-center px-5 mt-40">
             <span className="border rounded-full px-4 py-2 text-sm bg-linear-to-br from-cyan-400 to-green-800">2000+ jobs available</span>
             <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center mt-4 mb-4 font-semibold">Finding Your Dream <br /><span className="bg-linear-to-tr from-emerald-400 to-sky-400 text-transparent bg-clip-text">Software Engineering</span><br />Role Today</h1>
             <h2 className="text-xl sm:text-2xl md:text-3xl text-center mb-5 mt-5">Search through the latest opportunities in software engineering. Get hired fast with positions updated daily</h2>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 import { useLogout, useAuthenticatedUser } from "../hooks/useAuth";
 import { ChevronDown, House, TableOfContents, Handshake, Menu, X, BriefcaseBusiness, UserRoundArrowLeft } from 'lucide-react'
 import { motion, AnimatePresence } from 'motion/react'
@@ -7,12 +7,12 @@ import { motion, AnimatePresence } from 'motion/react'
 
 const Navbar = () => {
 
-    let parent = {
+    const parent = {
         hidden: { opacity: 0, y: -70 },
         visible: { opacity: 1, y: 0, transition: { staggerChildren: 0.25, duration: 0.7 } }
     }
 
-    let children = {
+    const children = {
         hidden: { opacity: 0, y: -70 },
         visible: { opacity: 1, y: 0 }
     }
@@ -61,13 +61,89 @@ const Navbar = () => {
                         </NavLink>
                 }
             </nav>
-            {
-                open && (
 
-                )
-            }
+            <AnimatePresence>
+                {open && (
+                    <motion.nav variants={parent} initial="hidden" animate="visible" exit={{ opacity: 0, y: 50 }} className='px-5'>
+                        <section className='px-5 hidden md:grid md:grid-cols-2 lg:grid-cols-3 justify-items-center mt-3'>
+                            <motion.div variants={children} className='space-y-4'>
+                                <label className='text-md font-bold tracking-tighter' htmlFor="resources">Resources</label>
+                                <div className='mt-3 flex space-x-4'>
+                                    <House size={20} />
+                                    <motion.div variants={children} whileHover={{ scale: 1.05 }}>
+                                        <NavLink className='text-sm font-semibold underline' onClick={close}>Blog</NavLink>
+                                        <h2 className='text-sm'>Read Industry Insights.</h2>
+                                    </motion.div>
+                                </div>
+
+                                <div className='mt-2 flex space-x-4'>
+                                    <TableOfContents size={20} />
+                                    <motion.div variants={children} whileHover={{ scale: 1.05 }}>
+                                        <Link to='/#faq' className='text-sm font-semibold underline' onClick={close}>FAQ</Link>
+                                        <h2 className='text-sm'>Common Questions Asked.</h2>
+                                    </motion.div>
+                                </div>
+
+                                <div className='mt-2 flex space-x-4'>
+                                    <Handshake />
+                                    <motion.div variants={children} whileHover={{ scale: 1.05 }}>
+                                        <NavLink className='text-sm font-semibold underline' onClick={close}>Support</NavLink>
+                                        <h2 className='text-sm'>We are here to help.</h2>
+                                    </motion.div>
+                                </div>
+                            </motion.div>
+
+                            <motion.div variants={children} className='space-y-4'>
+                                <label className='text-md font-bold tracking-tighter' htmlFor="insights">Latest Insights</label>
+                                <div className='mt-3 flex space-x-3'>
+                                    <motion.div variants={children} whileHover={{ scale: 1.05 }} className='p-10 w-50 rounded-xl bg-[url("https://cdn.pixabay.com/photo/2019/07/14/16/27/pen-4337521_1280.jpg")] bg-cover bg-center bg-no-repeat'>
+                                    </motion.div>
+                                    <div>
+                                        <h1 className='text-sm font-semibold'>Finding Your Next Role</h1>
+                                        <h2 className='text-sm'>Tips for landing positions.</h2>
+                                        <motion.div variants={children} whileHover={{ scale: 1.05 }}>
+                                            <NavLink to='/blogpost' className='text-sm underline' onClick={close}>Read More</NavLink>
+                                        </motion.div>
+                                    </div>
+                                </div>
+
+                                <div className='mt-3 flex space-x-3'>
+                                    <motion.div variants={children} whileHover={{ scale: 1.05 }} className='w-50 p-10 bg-gray-400 rounded-xl bg-[url("https://cdn.pixabay.com/photo/2023/12/15/11/13/programming-8450423_1280.png")] bg-cover bg-center bg-no-repeat'>
+                                    </motion.div>
+                                    <div>
+                                        <h1 className='text-sm font-semibold'>Remote Work Trends</h1>
+                                        <h2 className='text-sm'>What companies are hiring.</h2>
+                                        <motion.div variants={children} whileHover={{ scale: 1.05 }}>
+                                            <NavLink to='/jobpost' className='text-sm underline' onClick={close}>Read More</NavLink>
+                                        </motion.div>
+                                    </div>
+                                </div>
+
+                            </motion.div>
+
+                            <motion.div variants={children} className='space-y-4'>
+                                <label className='text-md font-bold tracking-tighter' htmlFor="contact">Contact</label>
+                                <div>
+                                    <h2 className='text-sm'>Get In touch with us.</h2>
+                                </div>
+
+                                <div className=' text-sm flex flex-col'>
+                                    <label htmlFor="email">Email</label>
+                                    <motion.span variants={children} whileHover={{ scale: 1.05 }} className='font-semibold'>nothtekayjay@icloud.com</motion.span>
+                                </div>
+
+
+                                <div className=' text-sm flex flex-col'>
+                                    <label htmlFor="mobile">Telephone</label>
+                                    <motion.span variants={children} whileHover={{ scale: 1.05 }} className='font-semibold'>+44 (0)7931 55 8921</motion.span>
+                                </div>
+                            </motion.div>
+                        </section>
+                    </motion.nav>
+
+                )}
+            </AnimatePresence>
         </header>
     )
 }
-
 export default Navbar
