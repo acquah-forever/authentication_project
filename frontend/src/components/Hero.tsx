7600f637749a24596ae08b4af7f5ebf411228e65
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-27 14:24:38 +0000

    added hero section

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+
+const Hero = () => {
+    return (
+        <div className="flex flex-col justify-center items-center px-5 min-h-screen">
+            <span className="border rounded-full px-4 py-2 text-sm bg-linear-to-br from-cyan-400 to-green-800">2000+ jobs available</span>
+            <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center mt-4 mb-4 font-semibold">Finding Your Dream <br /><span className="bg-linear-to-tr from-emerald-400 to-sky-400 text-transparent bg-clip-text">Software Engineering</span><br />Role Today</h1>
+            <h2 className="text-xl sm:text-2xl md:text-3xl text-center mb-5 mt-5">Search through the latest opportunities in software engineering. Get hired fast with positions updated daily</h2>
+            <div className='flex gap-2'>
+                <button className="bg-linear-to-br from-sky-300 to-sky-700 px-4 py-2 rounded border-2 border-white cursor-pointer">Search</button>
+                <button className="bg-linear-to-br from-emerald-400 to-emerald-700 px-4 py-2 rounded border-2 border-slate-500 cursor-pointer">Browse</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,19 +1,45 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom";
+import { useLogout, useAuthenticatedUser } from "../hooks/useAuth";
+import { ChevronDown, House, TableOfContents, Handshake,  Menu, X, BriefcaseBusiness, UserRoundArrowLeft } from 'lucide-react'
+
 
 const Navbar = () => {
+    const { mutate } = useLogout()
+    const { data: user, isLoading, isPending } = useAuthenticatedUser();
+    const navigate = useNavigate()
+
+    function handleClick() {
+        mutate(undefined, {
+            onSuccess: () => {
+                navigate("/login");
+            },
+        });
+    }
+
     return (
-        <nav className="flex justify-between items-center px-5 py-3">
+        <nav className="flex justify-between items-center px-5 py-5">
             <NavLink to="/" className="cursor-pointer text-lg">
-                Home
+                <House className="cursor-pointer flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300" size={34} />
             </NavLink>
             <ul className="flex space-x-5">
-                <li className="text-md cursor-pointer">About</li>
-                <li className="text-md cursor-pointer">Services</li>
-                <li className="text-md cursor-pointer">Contact</li>
+                <li className="text-md cursor-pointer cursor-pointer flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300">About</li>
+                <li className="text-md cursor-pointer cursor-pointer flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300">Jobs</li>
+                <button className="text-md cursor-pointer gap-0.5 flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300">
+                    More
+                    <ChevronDown className="mt-1" size={18} />
+                </button>
             </ul>
-            <NavLink className="cursor-pointer bg-linear-to-br from-sky-400 to-sky-800 px-5 py-2 rounded-sm border-2 border-white" to="/login">
-                    Log In
-            </NavLink>
+            {
+                !isLoading && user ?
+                    <button className="cursor-pointer bg-linear-to-br from-sky-400 to-sky-800 px-5 py-2 rounded-sm border-2 border-white" onClick={handleClick} disabled={isPending}>
+                        {isPending ? "Logging out..." : "Log Out"}
+                    </button>
+                    :
+                    <NavLink className="flex justify-center items-center gap-2 px-5 py-2 rounded-sm border-2 border-white cursor-pointer flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300"to="/login">
+                        <UserRoundArrowLeft />
+                        Log In
+                    </NavLink>
+            }
         </nav>
     )
 }
