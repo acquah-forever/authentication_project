4c013992721df12f2c0a76910b2659337355d999
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-28 12:55:18 +0000

    added hamburger menu

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -18,10 +18,11 @@ const Navbar = () => {
     }
 
     const { mutate } = useLogout()
-    const { data: user, isLoading, isPending } = useAuthenticatedUser();
+    const { data: user, isLoading } = useAuthenticatedUser();
     const navigate = useNavigate()
 
     const [open, setOpen] = useState<boolean>(false)
+    const [menu, setMenu] = useState<boolean>(false)
 
     function handleClick() {
         mutate(undefined, {
@@ -31,9 +32,16 @@ const Navbar = () => {
         });
     }
 
+    const toggle = (prev => !prev)
+
     function handleOpen(e: React.MouseEvent<HTMLButtonElement>) {
         e.preventDefault()
-        setOpen(prev => !prev)
+        setOpen(toggle)
+    }
+
+    function handleMenu(e: React.MouseEvent<HTMLButtonElement>){
+        e.preventDefault()
+        setMenu(toggle)
     }
 
     return (
@@ -42,7 +50,7 @@ const Navbar = () => {
                 <NavLink to="/" className="cursor-pointer text-lg">
                     <House className="cursor-pointer flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300" size={34} />
                 </NavLink>
-                <ul className="flex space-x-5">
+                <ul className="flex space-x-5 hidden sm:flex">
                     <li className="text-md cursor-pointer cursor-pointer flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300">About</li>
                     <li className="text-md cursor-pointer cursor-pointer flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300">Jobs</li>
                     <button className="text-md cursor-pointer gap-0.5 flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300" onClick={handleOpen}>
@@ -52,14 +60,19 @@ const Navbar = () => {
                 </ul>
                 {
                     !isLoading && user ?
-                        <button className="cursor-pointer bg-linear-to-br from-sky-400 to-sky-800 px-5 py-2 rounded-sm border-2 border-white" onClick={handleClick} disabled={isPending}>
-                            {isPending ? "Logging out..." : "Log Out"}
+                        <button className="hidden sm:flex cursor-pointer bg-linear-to-br from-sky-400 to-sky-800 px-5 py-2 rounded-sm border-2 border-white" onClick={handleClick}>
+                            Log Out
                         </button>
                         :
-                        <NavLink className="flex justify-center items-center gap-2 px-5 py-2 rounded-sm border-2 border-white cursor-pointer transition-all hover:scale-110 hover:underline duration-300" to="/login">
+                        <NavLink className="hidden sm:flex flex justify-center items-center gap-2 px-5 py-2 rounded-sm border-2 border-white cursor-pointer transition-all hover:scale-110 hover:underline duration-300" to="/login">
                             <UserRoundArrowLeft className='border bg-gray-500 rounded-full px-1' size={27} />
                             Log In
                         </NavLink>
+                }
+                {
+                    <div className="flex sm:hidden">
+                        menu ? (<X />) : (<Menu />)
+                    </div>
                 }
             </nav>
 
