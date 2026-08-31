3f53afc5c46aac861b3ceef8d4c2a1d13bcf1eff
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-31 19:10:05 +0000

    updated job model to includ enum for employment type and experience level

--- backend/src/models/jobs.ts
+++ backend/src/models/jobs.ts
@@ -2,10 +2,12 @@ import { model, InferSchemaType, Schema } from "mongoose";
 
 const jobsSchema = new Schema({
     
-    jobTitle: {type: String},
-    jobLocation: {type: String},
-    employmentType: {type: String},
-    experinceLevel: {type: String},
+    jobTitle: {type: String, required: true},
+    company : {type: String, required: true},
+    jobLocation: {type: String, required:true},
+    employmentType: {type: String, required:true, enum: ["Part-Time","Full-Time","Contract","Volunteer"]},
+    experinceLevel: {type: String, required:true, enum: ["Entry-Level","Junior","Senior","Manager"]},
+    requirements: {type: String},
     jobDescription: {type: String}
 })
 
